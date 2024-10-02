c168c51df115041f1cb23b311f44552a3daa41fc
Riskoder <g.vidal1g+1@gmail.com>
2024-10-02 17:43:50 -0300

    Template form for the creation of the Folder article

--- src/components/todoFolderTemplate.js
+++ src/components/todoFolderTemplate.js
@@ -1,76 +1,100 @@
-import { createIcon, createButton } from './Buttons';
-import { Folder } from '../storage';
+// import { createButton } from './Buttons';
+// import { createIcon } from './icon.js';
+// import { Folder } from '../storage/storeTodoFolder.js';
 
-const folderSection = document.getElementById('todo-projects-list');
-const addTodoFolder = document.getElementById('add-project');
+// const folderSection = document.getElementById('todo-projects-list');
+// const addTodoFolder = document.getElementById('add-project');
 
-export function createFolderForm() {
-  let isActive = false;
-  addTodoFolder.addEventListener('click', () => {
-    if (isActive) return;
+// export function createFolderForm() {
+//   let isActive = false;
+//   addTodoFolder.addEventListener('click', () => {
+//     if (isActive) return;
 
-    isActive = true;
+//     isActive = true;
 
-    const form = document.createElement('form');
-    form.action = '#';
-    form.classList.add('todo-folder', 'flex-column', 'flex-center');
-    form.autocomplete = 'off';
+//     const form = document.createElement('form');
+//     form.action = '#';
+//     form.classList.add('todo-folder', 'flex-column', 'flex-center');
+//     form.autocomplete = 'off';
 
-    const folderInputTitle = document.createElement('input');
-    folderInputTitle.maxLength = '14';
-    folderInputTitle.id = 'folder-title';
-    folderInputTitle.type = 'text';
-    folderInputTitle.classList.add('input-title');
-    folderInputTitle.placeholder = 'Add a Title';
+//     const folderInputTitle = document.createElement('input');
+//     folderInputTitle.maxLength = '14';
+//     folderInputTitle.id = 'folder-title';
+//     folderInputTitle.type = 'text';
+//     folderInputTitle.classList.add('input-title');
+//     folderInputTitle.placeholder = 'Add a Title';
 
-    const divButtonContainer = document.createElement('div');
+//     const folderInputDescription = document.createElement('input');
+//     folderInputDescription.maxLength = '100';
+//     folderInputDescription.type = 'text';
+//     folderInputDescription.id = 'folder-desc';
+//     folderInputDescription.classList.add('input-title');
+//     folderInputDescription.placeholder = 'Add a Description';
 
-    const addFolderButton = createButton('add');
+//     const divButtonContainer = document.createElement('div');
 
-    addFolderButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      addForm(folderInputTitle.value);
-      const newFolder = new Folder(folderInputTitle.value);
-      Folder.addFolder(newFolder);
-      folderSection.removeChild(form);
-      form.reset();
-      isActive = false;
-    });
+//     const addFolderButton = createButton('add');
 
-    const cancelFolderButton = createButton('cancel');
+//     addFolderButton.addEventListener('click', (e) => {
+//       e.preventDefault();
+//       addForm(folderInputTitle.value.trim());
+//       const newFolder = new Folder(
+//         folderInputTitle.value.trim(),
+//         folderInputDescription.value.trim()
+//       );
+//       Folder.addFolder(newFolder);
+//       folderSection.removeChild(form);
+//       form.reset();
+//       createMainContent();
+//       isActive = false;
+//     });
 
-    cancelFolderButton.addEventListener('click', () => {
-      folderSection.removeChild(form);
-      isActive = false;
-    });
+//     const cancelFolderButton = createButton('cancel');
 
-    divButtonContainer.appendChild(addFolderButton);
-    divButtonContainer.appendChild(cancelFolderButton);
+//     cancelFolderButton.addEventListener('click', () => {
+//       folderSection.removeChild(form);
+//       isActive = false;
+//     });
 
-    form.appendChild(folderInputTitle);
-    form.appendChild(divButtonContainer);
+//     divButtonContainer.appendChild(addFolderButton);
+//     divButtonContainer.appendChild(cancelFolderButton);
 
-    folderSection.appendChild(form);
-    folderInputTitle.focus();
-  });
-}
+//     form.appendChild(folderInputTitle);
+//     form.appendChild(folderInputDescription);
+//     form.appendChild(divButtonContainer);
 
-function addForm(title) {
-  const projectFolder = document.createElement('article');
-  projectFolder.classList.add('todo-folder', 'flex');
+//     folderSection.appendChild(form);
+//     folderInputTitle.focus();
+//   });
+// }
 
-  const projectTitle = document.createElement('span');
-  projectTitle.classList.add('todo-folder-name');
-  projectTitle.textContent = title;
+// function addForm(title) {
+//   const projectFolder = document.createElement('article');
+//   projectFolder.classList.add('todo-folder', 'flex', 'folderArticle');
 
-  const projectDeleteButton = createIcon('delete');
-  projectDeleteButton.addEventListener('click', () => {
-    Folder.deleteFolder(title);
-    folderSection.removeChild(projectFolder);
-  });
+//   const projectTitle = document.createElement('span');
+//   projectTitle.classList.add('todo-folder-name');
+//   projectTitle.textContent = title;
 
-  projectFolder.appendChild(projectTitle);
-  projectFolder.appendChild(projectDeleteButton);
+//   const projectDeleteButton = createIcon('delete');
+//   projectDeleteButton.addEventListener('click', () => {
+//     Folder.deleteFolder(title);
+//     folderSection.removeChild(projectFolder);
+//   });
 
-  folderSection.appendChild(projectFolder);
-}
+//   projectFolder.appendChild(projectTitle);
+//   projectFolder.appendChild(projectDeleteButton);
+
+//   folderSection.appendChild(projectFolder);
+// }
+
+// export function createMainContent() {
+//   const selectFolder = document.querySelectorAll('.folderArticle');
+
+//   selectFolder.forEach((folder, folderIndex) => {
+//     folder.addEventListener('click', () => {
+//       selectFolder.forEach((f) => f.classList.remove('active-folder'));
+//       folder.classList.add('active-folder');
+//     });
+//   });
+// }
